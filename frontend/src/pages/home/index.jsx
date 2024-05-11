03c70159d1f6bed5f6682a93ebd8c7fa405e22ce
dh ch <chaalaadhif@gmail.com>
2024-05-11 20:20:30 +0200

    project done

--- frontend/src/pages/home/index.jsx
+++ frontend/src/pages/home/index.jsx
@@ -12,104 +12,110 @@ import RadarPerformance from "../../components/radarPerformance/radarPerformance
 import RadialScore from "../../components/radialScore/radialScore";
 
 import {
-    getUser,
-    getUserActivity,
-    getUserPerformance,
-    getUserAverageSessions,
+     getUser,
+     getUserActivity,
+     getUserPerformance,
+     getUserAverageSessions,
 } from "../../services/service";
-import { useParams, Navigate } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 
 function Home() {
-    const { id } = useParams();
-    const [userId, setUserId] = useState(id);
-    const [userData, setUserData] = useState(null);
-    const [activityData, setActivityData] = useState([]);
-    const [performanceData, setPerformanceData] = useState([]);
-    const [sessionLengthData, setSessionData] = useState([]);
+     const { id } = useParams();
+     const navigate = useNavigate();
+     const [userId, setUserId] = useState(id);
+     const [userData, setUserData] = useState(null);
+     const [activityData, setActivityData] = useState([]);
+     const [performanceData, setPerformanceData] = useState([]);
+     const [sessionLengthData, setSessionData] = useState([]);
 
-    useEffect(() => {
-        if (!id) return <Navigate to="*" />;
+     useEffect(() => {
+          // Redirige si l'id est différent de 12 ou 18
+          if (userId !== "12" && userId !== "18") {
+               navigate("*");
+          }
 
-        const fetchData = async () => {
-            setUserId(id);
-            try {
-                const user = await getUser(userId);
-                //console.log("user:", user);
-                const activity = await getUserActivity(userId);
-                //console.log("activity data:", activity);
-                const performance = await getUserPerformance(userId);
-                //console.log("performance data:", performance);
-                const sessionLength = await getUserAverageSessions(userId);
-                //console.log("Session length data:", sessionLength);
-                setUserData(user);
-                setActivityData(activity);
-                setPerformanceData(performance);
-                setSessionData(sessionLength);
-            } catch (error) {
-                console.error("Error fetching data:", error);
-            }
-        };
-        fetchData();
-    }, []);
+          const fetchData = async () => {
+               setUserId(id);
+               try {
+                    const user = await getUser(userId);
+                    //console.log("user:", user);
+                    const activity = await getUserActivity(userId);
+                    //console.log("activity data:", activity);
+                    const performance = await getUserPerformance(userId);
+                    //console.log("performance data:", performance);
+                    const sessionLength = await getUserAverageSessions(userId);
+                    //console.log("Session length data:", sessionLength);
+                    setUserData(user);
+                    setActivityData(activity);
+                    setPerformanceData(performance);
+                    setSessionData(sessionLength);
+               } catch (error) {
+                    console.error("Error fetching data:", error);
+               }
+          };
+          fetchData();
+     }, []);
 
-    const icons = [
-        {
-            logo: calorie,
-            unit: "KCal",
-            title: "Calories",
-            backgroundColor: "#Fbeaea",
-            keyData: userData ? userData.keyData.calorieCount / 1000 : 0,
-        },
-        {
-            logo: protein,
-            unit: "g",
-            title: "Proteins",
-            backgroundColor: "#4AB8FF1A",
-            keyData: userData ? userData.keyData.proteinCount : 0,
-        },
-        {
-            logo: glucide,
-            unit: "g",
-            title: "Glucides",
-            backgroundColor: "#fbf6e5",
-            keyData: userData ? userData.keyData.carbohydrateCount : 0,
-        },
-        {
-            logo: lipide,
-            unit: "g",
-            title: "Lipides",
-            backgroundColor: "#FD51811A",
-            keyData: userData ? userData.keyData.lipidCount : 0,
-        },
-    ];
+     const icons = [
+          {
+               logo: calorie,
+               unit: "KCal",
+               title: "Calories",
+               backgroundColor: "#Fbeaea",
+               keyData: userData ? userData.keyData.calorieCount / 1000 : 0,
+          },
+          {
+               logo: protein,
+               unit: "g",
+               title: "Proteins",
+               backgroundColor: "#4AB8FF1A",
+               keyData: userData ? userData.keyData.proteinCount : 0,
+          },
+          {
+               logo: glucide,
+               unit: "g",
+               title: "Glucides",
+               backgroundColor: "#fbf6e5",
+               keyData: userData ? userData.keyData.carbohydrateCount : 0,
+          },
+          {
+               logo: lipide,
+               unit: "g",
+               title: "Lipides",
+               backgroundColor: "#FD51811A",
+               keyData: userData ? userData.keyData.lipidCount : 0,
+          },
+     ];
 
-    return (
-        <div className="home">
-            <Header userData={userData} />
-            <div className="row">
-                <div className="diagramsColumn">
-                    <BarActivity sessions={activityData} />
-                    <div className="diagramsRow">
-                        <LinePeriod sessionLength={sessionLengthData} />
-                        <RadarPerformance performance={performanceData} />
-                        <RadialScore score={userData ? userData.score : 0} />
+     return (
+          <div className="home">
+               <Header userData={userData} />
+               <div className="row">
+                    <div className="diagramsColumn">
+                         <BarActivity sessions={activityData} />
+                         <div className="diagramsRow">
+                              <LinePeriod sessionLength={sessionLengthData} />
+                              <RadarPerformance performance={performanceData} />
+                              <RadialScore
+                                   score={userData ? userData.score : 0}
+                              />
+                         </div>
                     </div>
-                </div>
-                <div className="aside">
-                    {icons.map((icon) => (
-                        <Icon
-                            key={icon.title}
-                            logo={icon.logo}
-                            value={icon.keyData}
-                            unit={icon.unit}
-                            title={icon.title}
-                            backgroundColor={icon.backgroundColor}
-                        />
-                    ))}
-                </div>
-            </div>
-        </div>
-    );
+                    <div className="aside">
+                         {icons.map((icon) => (
+                              <Icon
+                                   key={icon.title}
+                                   logo={icon.logo}
+                                   value={icon.keyData}
+                                   unit={icon.unit}
+                                   title={icon.title}
+                                   backgroundColor={icon.backgroundColor}
+                              />
+                         ))}
+                    </div>
+               </div>
+          </div>
+     );
 }
 
 export default Home;
--- frontend/src/services/service.js
+++ frontend/src/services/service.js
@@ -1,71 +1,58 @@
 import axios from "axios";
 import {
-    USER_MAIN_DATA,
-    USER_ACTIVITY,
-    USER_AVERAGE_SESSIONS,
-    USER_PERFORMANCE,
+     USER_MAIN_DATA,
+     USER_ACTIVITY,
+     USER_AVERAGE_SESSIONS,
+     USER_PERFORMANCE,
 } from "./mockData";
-let isDataMocked = true;
+let isDataMocked = false;
 const API_URL = "http://localhost:3000";
+
 export async function getUser(userId) {
-    if (isDataMocked) {
-        let data = fetchDataMocked(USER_MAIN_DATA, userId);
-        return data;
-    } else {
-        let res = await fetchData(`/user/${userId}`).data;
-        return res.data;
-    }
+     if (isDataMocked) {
+          let data = fetchDataMocked(USER_MAIN_DATA, userId);
+          return data;
+     } else {
+          let res = await fetchData(`/user/${userId}`);
+          return res;
+     }
 }
 export async function getUserActivity(userId) {
-    /*  if (isDataMocked) {
-        let data = fetchDataMocked(USER_ACTIVITY, userId);
-        return data;
-    } else */
-    {
-        let res = await fetchData(`/user/${userId}/activity`);
-        console.log(res);
-        return res;
-    }
-}
-export async function getUserAverageSessions(userId) {
-    if (isDataMocked) {
-        let data = fetchDataMocked(USER_AVERAGE_SESSIONS, userId);
-        return data;
-    } else return fetchData(`/user/${userId}/average-sessions`).sessions;
-}
-export async function getUserPerformance(userId) {
-    if (isDataMocked) {
-        let data = fetchDataMocked(USER_PERFORMANCE, userId);
-        return data;
-    } else return fetchData(`/user/${userId}/performance`).kind;
-}
-{
-    /*async function fetchData(endpoint) {
-    try {
-        const promise = await fetch(`http://localhost:3000${endpoint}`, {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-            },
-        });
-        const dataJson = await promise.json();
-        //console.log(dataJson);
-        return dataJson.data;
-    } catch (error) {
-        throw new Error(`Failed to fetch data from ${endpoint}`);
-    }
-}*/
-}
-async function fetchData(endpoint) {
-    try {
-        const res = await axios.get(`http://localhost:3000${endpoint}`);
-        console.log(res);
-        return res;
-    } catch (error) {
-        console.log(error);
-    }
+     if (isDataMocked) {
+          let data = fetchDataMocked(USER_ACTIVITY, userId);
+          return data;
+     } else {
+          let res = await fetchData(`/user/${userId}/activity`);
+          return res;
+     }
 }
 
+export async function getUserAverageSessions(userId) {
+     if (isDataMocked) {
+          let data = fetchDataMocked(USER_AVERAGE_SESSIONS, userId);
+          return data;
+     } else {
+          let res = await fetchData(`/user/${userId}/average-sessions`);
+          return res;
+     }
+}
+export async function getUserPerformance(userId) {
+     if (isDataMocked) {
+          let data = fetchDataMocked(USER_PERFORMANCE, userId);
+          return data;
+     } else {
+          let res = await fetchData(`/user/${userId}/performance`);
+          return res;
+     }
+}
+async function fetchData(endpoint) {
+     try {
+          const res = await axios.get(`${API_URL}${endpoint}`);
+          return res?.data?.data;
+     } catch (error) {
+          console.log(error);
+     }
+}
 export function fetchDataMocked(key, userId) {
-    return key.find((element) => element.userId == userId);
+     return key.find((element) => element.userId == userId);
 }
